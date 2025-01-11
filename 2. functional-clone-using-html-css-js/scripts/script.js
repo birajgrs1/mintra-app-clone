171e7b3b4c86045d4bc9836d7ace89821edd4ceb
birajgrs <birajghorasaine11@gmail.com>
2025-01-11 16:34:23 +0545

    Project complete using Javascript functionality also  ...

--- 2. functional-clone-using-html-css-js/scripts/script.js
+++ 2. functional-clone-using-html-css-js/scripts/script.js
@@ -1,5 +1,3 @@
-let itemsContainerElement = document.querySelector(".items-container");
-
 /*
 let item = {
   item_image: "./images/1.jpg",
@@ -15,41 +13,57 @@ let item = {
 };
 */
 
-let bagItems = [];
-displayItemsOnHomePage();
+// let bagItems = [];
+onLoad();
 
-function addToBag(itemId) {
-  bagItems.push(itemId);
-  // console.log(`${itemId} object clicked.`);
+function onLoad() {
+  let bagItemsStr = localStorage.getItem("bagItems");
+  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+  displayItemsOnHomePage();
+  displayBagIcon();
 }
 
+function addToBag(itemId) {
+  if (!bagItems.includes(itemId)) {
+    bagItems.push(itemId);
+    localStorage.setItem("bagItems", JSON.stringify(bagItems));
+  }
+  displayBagIcon();
+}
+
+function displayBagIcon() {
+  let bagItemCountElement = document.querySelector('.bag-item-count');
+  if (bagItems.length > 0) {
+    bagItemCountElement.style.visibility = 'visible';
+    bagItemCountElement.innerText = bagItems.length;
+  } else {
+    bagItemCountElement.style.visibility = 'hidden';
+  }
+}
+
+
 function displayItemsOnHomePage() {
-  let innerHTML = "";
-  items.forEach((item) => {
-    innerHTML += `
-
-        <div class="item-container">
-          <img class="item-img" src="${item.image}" alt="image not found">
-          <div class="rating">
-            ${item.rating.stars} ⭐ | ${item.rating.count}
-          </div>
-          <div class="company-name">
-          ${item.company}
-          </div>
-          <div class="item-name">
-            ${item.item_name}
-          </div>
-
-          <div class="price">
-            <span class="current-price">Rs ${item.current_price}</span>
-            <span class="original-price"> Rs ${item.original_price}</span>
-            <span class="discount"> (${item.discount_percentage}% off)</span>
-          </div>
-          <button class="add-to-bag" onClick = 'addToBag(${item.id})'>Add to Bag</button>
-        </div>
-   
-    `;
+  let itemsContainerElement = document.querySelector('.items-container');
+  if (!itemsContainerElement) {
+    return;
+  }
+  let innerHtml = '';
+  items.forEach(item => {
+    innerHtml += `
+    <div class="item-container">
+      <img class="item-image" src="${item.image}" alt="item image">
+      <div class="rating">
+          ${item.rating.stars} ⭐ | ${item.rating.count}
+      </div>
+      <div class="company-name">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="price">
+          <span class="current-price">Rs ${item.current_price}</span>
+          <span class="original-price">Rs ${item.original_price}</span>
+          <span class="discount">(${item.discount_percentage}% OFF)</span>
+      </div>
+      <button class="add-to-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+    </div>`
   });
-
-  itemsContainerElement.innerHTML = innerHTML;
+  itemsContainerElement.innerHTML = innerHtml;
 }
