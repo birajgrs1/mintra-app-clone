d0a5682106368356e7974a4d2939928584abba00
birajgrs <birajghorasaine11@gmail.com>
2025-01-29 18:06:48 +0545

    starting project by comying basic needed parts from old clone to new app and fixed basic ui parts   ....

--- 3. mintra-clone-ui-reactjs/final-clone-reactjs/src/App.jsx
+++ 3. mintra-clone-ui-reactjs/final-clone-reactjs/src/App.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeItem from "./components/HomeItem";
 
 function App() {
+  const items = [
+    {
+      id: "001",
+      image: "images/1.jpg",
+      company: "Carlton London",
+      item_name: "Rhodium-Plated CZ Floral Studs",
+      original_price: 1045,
+      current_price: 606,
+      discount_percentage: 42,
+      return_period: 14,
+      delivery_date: "10 Oct 2023",
+      rating: {
+        stars: 4.5,
+        count: 1400,
+      },
+    },
+  ];
 
   return (
     <>
-    <h2>This is Demo clone.</h2>
+      <Header />
+
+      <main>
+        <div className="items-container">
+          {items.map((item) => (
+            <HomeItem key={item.id} item={item} />
+          ))}
+        </div>
+      </main>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
